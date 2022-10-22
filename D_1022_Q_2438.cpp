44f61c4096f1e377f2d2322b9325a2c5b12f6a1e
Adle0na <uuuuufgh@gmail.com>
2022-10-23 00:02:28 +0900

    [test] First commit Backjoon Study Q_2438 Clear!
    별찍기1

---
+++ D_1022_Q_2438.cpp
@@ -0,0 +1,19 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+
+int main()
+{
+	int N;
+	
+	scanf("%d", &N);
+
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j <= i; j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+}
