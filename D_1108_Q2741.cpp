fb302c6bfbc489154b828ade7f2e6b5d9219bee1
Adle0na <uuuuufgh@gmail.com>
2022-11-10 00:05:38 +0900

    [feat] Backjoon Study Q_2741 Clear!

---
+++ D_1108_Q2741.cpp
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+	int n;
+
+	scanf("%d", &n);
+
+	for (int i = 1; i <= n; i++)
+	{
+		printf("%d\n", i);
+	}
+	return 0;
+}
