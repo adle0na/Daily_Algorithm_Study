a611f36017e9a606cd7181ac3994aea0977ebaae
Adle0na <uuuuufgh@gmail.com>
2022-11-23 00:30:23 +0900

    [feat] Backjoon Study Q_10797, 9295 Clear!

---
+++ D_1122_Q9295.cpp
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <stdio.h>
+#include <algorithm>
+#include <vector>
+#include <numeric>
+using namespace std;
+
+int main(void)
+{
+	// 이거쓰면 C++만의 독립적인 버퍼형성으로 속도 향상
+	// 단점으로는 멀티 쓰레드 환경에서는 출력 순서 보장 X C의 scanf, gets, getchar와 사용 불가
+	ios_base::sync_with_stdio(0);
+
+	// 이거 쓰면 cin과 cout의 묶음을 풀어 순서가 사라진다
+	// 화면에 보이는 순서가 중요하지 않은 알고리즘 풀때만 써야한다
+	cin.tie(0);
+
+	int case_num, sum = 0;
+	
+	cin >> case_num;
+
+	for (int i = 0; i < case_num; i++)
+	{
+		int num1, num2;
+
+		cin >> num1 >> num2;
+
+		sum += (num1 + num2);
+
+		cout << "Case " << i + 1 << ": " << sum << "\n";
+
+		sum = 0;
+	}
+
+	return 0;
+}
