d7c3906cb1dec67eb3aadbb9e35d85f18dcfcf3d
adle0na <44155920+adle0na@users.noreply.github.com>
2023-01-17 23:42:30 +0900

    [level 0] Title: 특이한 정렬, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.cpp
+++ 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.cpp
@@ -1,31 +1,22 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <cmath>
 
 using namespace std;
 
-bool compare(pair<int, int> a, pair<int, int> b)
+int num;
+
+bool compare(int prev, int next)
 {
-    if(a.first == b.first) return a.second > b.second;
+    if (abs(prev - num) == abs(next - num)) 
+        return prev > next;
     
-    return a.first < b.first;
+    return abs(prev - num) < abs(next - num);
 }
 
 vector<int> solution(vector<int> numlist, int n) {
-    vector<int> answer;
-    
-    vector<pair<int, int>> v;
-    
-    sort(numlist.begin(), numlist.end(), greater<>());
-    
-    for(int i = 0; i < numlist.size(); i++)
-        v.push_back(make_pair(abs(numlist[i]-n), numlist[i]));
-    
-    sort(v.begin(), v.end(), compare);
-    
-    for(int i = 0; i < v.size(); i++)
-        answer.push_back(v[i].second);
-    
-    return answer;
-
+    num = n;
+    sort(numlist.begin(), numlist.end(), compare);
+    return numlist;
 }
