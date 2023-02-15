0b92367482044e4541197491e84a076ef9b2855a
adle0na <44155920+adle0na@users.noreply.github.com>
2023-02-15 23:11:35 +0900

    [Silver IV] Title: 접미사 배열, Time: 8 ms, Memory: 2560 KB -BaekjoonHub

---
+++ 백준/Silver/11656. 접미사 배열/접미사 배열.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    string s;
+    string temp;
+    vector<string> v;
+
+    cin >> s;
+
+    for(int i = 0; i < s.length(); i++)
+    {
+        temp = s.substr(i, s.length() - i);
+        v.push_back(temp);
+    }
+
+    sort(v.begin(), v.end());
+
+    for(int i = 0; i < v.size(); i++)
+    {
+        cout << v[i] << "\n";
+    }
+}
