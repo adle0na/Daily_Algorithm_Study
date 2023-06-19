3752bc26df7e1178d95b9285d9ad5d466e019a76
adle0na <44155920+adle0na@users.noreply.github.com>
2023-06-19 23:37:07 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	int a, b;
+
+	cin >> a >> b;
+
+	cout << a + b << "\n";
+	cout << a - b << "\n";
+	cout << a * b << "\n";
+	cout << a / b << "\n";
+	cout << a % b;
+
+	return 0;
+}
