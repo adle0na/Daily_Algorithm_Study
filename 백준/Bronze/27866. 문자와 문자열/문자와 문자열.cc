868961fbbfbe0f6deaedf8dc08eb975755721aa5
adle0na <44155920+adle0na@users.noreply.github.com>
2025-05-12 11:45:08 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string S;
+    int i;
+    cin >> S >> i;
+
+    cout << S[i - 1] << '\n';
+    return 0;
+}
