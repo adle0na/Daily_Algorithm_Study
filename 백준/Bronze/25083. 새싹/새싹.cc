bf35e1a4e31650c99e878853b4659ea1c4752c11
adle0na <44155920+adle0na@users.noreply.github.com>
2025-05-16 10:30:20 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    // 글자 ' 자체를 넣고 싶다	\'
+    // 글자 " 자체	\"
+    // 글자 \ 자체	\\
+
+    string s;
+
+    cout << R"(         ,r'"7
+r`-_   ,'  ,/
+ \. ". L_r'
+   `~\/
+      |
+      |
+)";
+
+    return 0;
+}
