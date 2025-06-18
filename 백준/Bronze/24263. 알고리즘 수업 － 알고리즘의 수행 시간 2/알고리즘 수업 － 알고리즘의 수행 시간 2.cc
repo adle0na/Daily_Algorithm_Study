10015bee567f98a80917c589e50381bf9f8f7c7a
adle0na <44155920+adle0na@users.noreply.github.com>
2025-06-18 13:51:19 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    cout << n << '\n'; // 수행 횟수
+    cout << 1 << '\n'; // 최고차항 차수 (O(1) -> 0차)
+    return 0;
+}
